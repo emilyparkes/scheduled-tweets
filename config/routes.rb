c4737803d33c829b2b62b017c6bd1550f25b2108
Emily Parkes <emilycoco@me.com>
2023-09-15 12:31:12 +1200

    root route & navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
   # Defines the root path route ("/")
+  
+  # get "/", to: "main#index" <--- alternative
+  root to: "main#index"
+
   # root "articles#index"
 end
+
+
